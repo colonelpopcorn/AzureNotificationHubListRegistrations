8069faf0b1f9ccfe3d0c0b55fa8d4d418c6cf6f3
Jonathan Ling <jcling@tva.gov>
2024-07-18 16:25:53 -0400

    Initial commit with list command.

---
+++ AzureListRegistrations/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Azure.NotificationHubs;
+using Newtonsoft.Json;
+
+var registrations = await NotificationHubClient.CreateClientFromConnectionString(args[0] as string, args[1] as string)
+    .GetAllRegistrationsAsync(1000);
+
+Console.WriteLine(JsonConvert.SerializeObject(registrations, Formatting.Indented).ToString());
